d7aaae60633eb994f6b00bec3ffb5c35d1a700dc
Dhruv-technology <77441492+Dhruv-technology@users.noreply.github.com>
2023-10-16 20:22:33 +0530

    Add files via upload

---
+++ perimeter.py
@@ -0,0 +1,13 @@
+y=int(input("enter length"))
+x=int(input("enter breadth"))
+print(2*y+2*x)
+I hate primary incharge
+school=["teacher,students,princi,bad VP,GoodVp,primary incharge"]
+or teacher in school:
+    print(school)
+tup=(1,5,6,7,9)
+print(tup)
+for charecter in "BShravan":
+    print(charecter,end=" ")
+for i in range(101,90,-2):
+    print(i,end="\t")
